6db58e24896251afe98cea7d9215337f8a277e4a
Ingars Jekabsons <Ingars.Jekabsons@tietoevry.com>
2022-11-08 11:34:27 +0200

    heroku compatible

--- app/Main.hs
+++ app/Main.hs
@@ -8,10 +8,13 @@ import Control.Monad.Logger (runStderrLoggingT, runStdoutLoggingT)
 import DB (migrateAll)
 import Database.Esqueleto.Experimental (runMigration)
 import Database.Persist.Sqlite (createSqlitePool, runSqlite)
+import System.Environment (getEnv)
 
 main :: IO ()
 main = do
   runSqlite "esdb.db" $ do
     runMigration migrateAll
   pool <- runStdoutLoggingT $ createSqlitePool "esdb.db" 4
-  liftIO $ runAPI pool "http://127.0.0.1:8888" 9999
+  eshopPort <- getEnv "PORT"
+  whURL <- getEnv "WH_URL"
+  liftIO $ runAPI pool whURL (read eshopPort)
